2fdd66e7bc2ff013e993830b5b703e87a5aa542a
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-05-14 23:16:09 +0530

    Add sign in functionality

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,18 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyC0XzyZuYxeQ_qSx5_VfMNzGRP1XTZvOlg',
-  authDomain: 'entertainment-app-59dbf.firebaseapp.com',
-  projectId: 'entertainment-app-59dbf',
-  storageBucket: 'entertainment-app-59dbf.appspot.com',
-  messagingSenderId: '719366376948',
-  appId: '1:719366376948:web:bf63d668db011532c43da1',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'entertainment-app-ee5cb.firebaseapp.com',
+  projectId: 'entertainment-app-ee5cb',
+  storageBucket: 'entertainment-app-ee5cb.appspot.com',
+  messagingSenderId: '1004715600987',
+  appId: '1:1004715600987:web:14a4c57007408858df237e',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+const auth = getAuth();
+
 export const db = getFirestore();
+
+export default auth;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import auth from '../firebase.config';
 import Logo from '../assets/logo.svg';
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    const newData = {
+      [e.target.id]: e.target.value,
+    };
+    setFormData((prev) => {
+      return {
+        ...prev,
+        ...newData,
+      };
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+
+      if (userCredentials.user) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="px-4 mt-12">
       <div className="mx-auto text-center w-max">
@@ -11,19 +47,25 @@ const SignIn = () => {
       <h1 className="my-6 text-4xl font-semibold text-center text-white">Welcome Back!</h1>
       <div className="w-full px-4 py-6 mt-8 text-white rounded-md shadow-md bg-semi_dark_blue sm:max-w-md sm:px-6 sm:mx-auto">
         <h2 className="my-3 text-2xl font-semibold text-white">Login</h2>
-        <form className="w-full my-6 mb-8">
+        <form className="w-full my-6 mb-8" onSubmit={onSubmit}>
           <input
             type="email"
             name="email"
             id="email"
+            value={email}
+            onChange={onChange}
             placeholder="example@email.com"
+            autoComplete="off"
             className="w-full py-2 pb-4 pl-2 mb-5 text-white bg-transparent border-b placeholder-greyish_blue border-greyish_blue outline-0 caret-logo_red focus:border-white"
           />
           <input
             type="password"
             name="password"
             id="password"
+            value={password}
+            onChange={onChange}
             placeholder="********"
+            autoComplete="off"
             className="w-full py-2 pb-4 pl-2 mb-5 text-white bg-transparent border-b placeholder-greyish_blue border-greyish_blue outline-0 caret-logo_red focus:border-white"
           />
           <button
